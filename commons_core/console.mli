953bb306343caa908a1c43dcbf61816499eafd2e
pad <yoann.padioleau@gmail.com>
2018-01-05 09:45:39 -0800

    * commons_core/console.mli: estet

--- commons_core/console.mli
+++ commons_core/console.mli
@@ -5,13 +5,10 @@ val progress:
   ?show:bool -> ((unit -> unit) -> 'a list -> 'b) -> 'a list -> 'b
 
 
-
 val execute_and_show_progress:
   show:bool -> int -> ((unit -> unit) -> 'a) -> unit
-
 val execute_and_show_progress2:
   ?show:bool -> int -> ((unit -> unit) -> 'a) -> 'a
 
 val with_progress_list_metter:
   ?show:bool -> ((unit -> unit) -> 'a list -> 'b) -> 'a list -> 'b
-
