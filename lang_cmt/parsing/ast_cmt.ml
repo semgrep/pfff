a87023608dc0b5c8af3e42068e2fbe92f037b555
pad <yoann.padioleau@gmail.com>
2018-01-09 11:36:23 -0800

    * lang_cmt/parsing/ast_cmt.ml: comment

--- lang_cmt/parsing/ast_cmt.ml
+++ lang_cmt/parsing/ast_cmt.ml
@@ -1,6 +1,6 @@
 (* Yoann Padioleau
  *
- * Copyright (C) 2012 Facebook
+ * Copyright (C) 2012, 2017 Facebook
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
@@ -17,10 +17,17 @@
 (* Prelude *)
 (*****************************************************************************)
 (* 
- * This is mostly a wrapper around the ocaml compiler libs and its
- * typed ocaml tree. See the typed_ml file in this directory for
- * a copy paste of what is in the ocaml compiler source (and
+ * This is mostly a wrapper around the OCaml compiler libs and its
+ * typed OCaml tree. See the ast_cmt_stdlib.ml file in this directory for
+ * a copy paste of what is in the OCaml compiler source (and what
  * was used to generate via ocamltarzan meta_ast_cmt.ml).
+ * 
+ * todo: starting from ocaml 4.02, you can use 
+ * https://github.com/ocaml-ppx/ocaml-migrate-parsetree
+ * to migrate automatically .cmt from one version to another.
+ * That way you can write code (e.g., in graph_code_cmt.ml) handling
+ * the AST of OCaml 4.02 and the same code could work on 4.06
+ * (by converting down the AST from 4.06 to 4.02).
  *)
 
 (*****************************************************************************)
