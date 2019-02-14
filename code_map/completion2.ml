75d2e1a16d3df7258434a84a97461d06e7a79555
pad <yoann.padioleau@gmail.com>
2019-02-14 15:11:43 +0100

    * code_map/completion2.ml: prototype of pixbuf icon and coloring of entities in search completion box

--- code_map/completion2.ml
+++ code_map/completion2.ml
@@ -22,6 +22,7 @@ module E = Entity_code
 module Db = Database_code
 module BG = Big_grep
 module Flag = Flag_visual
+module HC = Highlight_code
 
 (*****************************************************************************)
 (* Prelude *)
@@ -37,7 +38,7 @@ module Flag = Flag_visual
 (*****************************************************************************)
 
 (*****************************************************************************)
-(*  *)
+(* Code indexing *)
 (*****************************************************************************)
 
 (*s: build_completion_defs_index *)
@@ -50,12 +51,17 @@ let build_completion_defs_index all_entities =
   BG.build_index all_entities
 (*e: build_completion_defs_index *)
 
-
 (*****************************************************************************)
-(* Model *)
+(* Icons *)
 (*****************************************************************************)
 
-let icon_of_kind kind has_test =
+(* reference:
+ * http://help.eclipse.org/kepler/index.jsp?topic=/org.eclipse.jdt.doc.user/reference/ref-icons.htm
+ *)
+
+let pixbuf = GdkPixbuf.from_file "/home/pad/package.gif"
+
+let _icon_of_kind kind has_test =
   match kind with
   | E.Function -> 
       if has_test then `YES else `NO
@@ -72,6 +78,9 @@ let icon_of_kind kind has_test =
   | E.Global -> `MEDIA_RECORD
   | E.Method -> `CONVERT
 
+  (* less: could use github.com/file-icons/atom to give different icons
+   * depending on the filename
+   *)
   | E.File -> `FILE
   | E.Dir -> `DIRECTORY
   | E.MultiDirs -> `QUIT
@@ -87,16 +96,27 @@ let icon_of_kind kind has_test =
 
   | (E.TopStmts | E.Other _ ) -> raise Todo
 
-(* less: could get rid of, just used in model_of_list_pair_string_with_icon *)
-type t = { 
-  mutable id: int;
-  mutable entity: Database_code.entity;
-  mutable text: string; 
-  mutable file: string; 
-  mutable count: string;
-  mutable kind: string;
-  mutable icon: GtkStock.id;
-}
+(*****************************************************************************)
+(* Colors *)
+(*****************************************************************************)
+
+let color_of_entity_kind kind =
+  let fake_usedef2 = HC.Def2 (HC.NoUse) in
+  let props = HC.info_of_entity_kind_and_usedef2 kind fake_usedef2 in
+  let color = 
+    try props |> Common.find_some (function
+        | `FOREGROUND s -> Some s
+        | _ -> None
+      )
+    with Not_found -> 
+        failwith (spf "could not find a FOREGROUND color for entity %s"
+          (Entity_code.string_of_entity_kind kind))
+  in
+  color
+
+(*****************************************************************************)
+(* Model *)
+(*****************************************************************************)
 
 let column_list = new GTree.column_list
 (* old:
@@ -109,69 +129,64 @@ let col_id   = column_list#add Gobject.Data.int
 let col_text = column_list#add Gobject.Data.string
 let col_file = column_list#add Gobject.Data.string
 let col_count = column_list#add Gobject.Data.string
-let _col_kind = column_list#add Gobject.Data.string
-let col_icon = column_list#add GtkStock.conv
+let col_icon = column_list#add (Gobject.Data.gobject_by_name "GdkPixbuf")
+(* To colorize the foreground of col_text.
+ * less: could also colorize the background and could also colorize
+ * col_file (using dircolors.ml color coding)
+ *)
+let col_color = column_list#add Gobject.Data.string
 
 
 let model_of_list_pair_string_with_icon2 xs =
 
-  (* old: I was using originally a custom_list 
+  (* old: I was using before a custom_list 
    * (see custom_list_generic.ml in lablgtk2/examples)
-   * to optimize things because GTree.tree_store used to be really slow.
+   * to be faster because GTree.tree_store used to be really slow.
    * However, I am now unable to make it work with recent gtk/lablgtk2
    * (I get some failure assertions regarding GtkModelFilter)
-   * so I reverted back to using the simpler tree_store.
+   * so I reverted back to using the simpler tree_store which in 2019
+   * seems ok.
    *)
   let model = GTree.tree_store column_list in
   let store_id_to_entity = Hashtbl.create 101 in
 
   pr2 (spf "Size of model = %d" (List.length xs));
   xs |> Common2.index_list_0 |> List.iter (fun (e, id) ->
-    let kind = e.Db.e_kind in
-    
-    let has_unit_test =
+   
+    let _has_unit_test =
       List.length e.Db.e_good_examples_of_use >= 1
     in
     let name = e.Db.e_name in
+
     (* if the string is too long, we will not see the other properties *)
     let final_name = 
       try (String.sub name 0 30) ^ "..."
       with Invalid_argument _ -> name
     in
-    let t = { 
-      id = id;
-      entity = e;                     
+    let color = color_of_entity_kind e.Db.e_kind in
 
-      (* had originally an ugly hack where we would artificially create
-       * a text2 field with always set to query. Indeed
-       * gtk seems to be confused if the column referenced
-       * by set_text_column contains a string that is not matching
-       * the current query. So here we were building this fake text entry.
-       * In fact as explained on the pygtk entry of entry_completion
-       * you don't have to use set_text_column if you provide
-       * your own set_match_func, which we do.
-       * Maybe we should just not use Entrycompletion at all and build
-       * our own popup.
-       *)
-      text = final_name;
-
-
-      file = e.Db.e_file;
-      count = i_to_s (e.Db.e_number_external_users);
-      kind = E.string_of_entity_kind kind;
-      icon = icon_of_kind kind has_unit_test;
-    }
-    in 
-    (* custom_list#insert *)
     let row = model#append () in
 
-    model#set ~row ~column:col_id t.id;
-    model#set ~row ~column:col_text t.text;
-    model#set ~row ~column:col_count t.count;
-    model#set ~row ~column:col_file t.file;
-    model#set ~row ~column:col_icon t.icon;
+    model#set ~row ~column:col_id id;
+    (* I had originally an ugly hack where I would artificially create
+     * a text2 field with always set to query. Indeed
+     * gtk seems to be confused if the column referenced
+     * by set_text_column contains a string that is not matching
+     * the current query. So here I was building this fake text entry.
+     * In fact as explained on the pygtk entry of entry_completion
+     * you don't have to use set_text_column if you provide
+     * your own set_match_func, which I do now.
+     * Maybe we should just not use Entrycompletion at all and build
+     * our own popup.
+     *)
+    model#set ~row ~column:col_text final_name;
+    model#set ~row ~column:col_count (i_to_s (e.Db.e_number_external_users));
+    model#set ~row ~column:col_file e.Db.e_file;
+(* TODO icon_of_kind kind has_unit_test *)
+    model#set ~row ~column:col_icon pixbuf;
+    model#set ~row ~column:col_color color;
 
-    Hashtbl.add store_id_to_entity id t.entity;
+    Hashtbl.add store_id_to_entity id e;
   );
   model, store_id_to_entity
 
@@ -199,12 +214,14 @@ let add_renderer (completion : GEdit.entry_completion) =
   let renderer = GTree.cell_renderer_pixbuf [ `STOCK_SIZE `LARGE_TOOLBAR ] in
   completion#pack (renderer :> GTree.cell_renderer);
   completion#add_attribute (renderer :> GTree.cell_renderer) 
-    "stock_id" col_icon;
+    "pixbuf" col_icon;
 
   let renderer = GTree.cell_renderer_text [] in
   completion#pack (renderer :> GTree.cell_renderer);
   completion#add_attribute (renderer :> GTree.cell_renderer) 
     "text" col_text;
+  completion#add_attribute (renderer :> GTree.cell_renderer) 
+    "foreground" col_color;
 
   let renderer = GTree.cell_renderer_text [`WIDTH 80] in
   completion#pack (renderer :> GTree.cell_renderer);
@@ -215,7 +232,9 @@ let add_renderer (completion : GEdit.entry_completion) =
   completion#pack (renderer :> GTree.cell_renderer);
   completion#add_attribute (renderer :> GTree.cell_renderer) 
     "text" col_file;
+
   ()
+  
 
 let fake_entity = {Database_code.
      e_name = "foobar";
