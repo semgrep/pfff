33277ad1716a1428740e1461eb532ec0f2a69fd5
pad <yoann.padioleau@gmail.com>
2019-04-26 17:53:22 +0200

    * lang_js/parsing/parsing_hacks_js.ml: misc

--- lang_js/parsing/parsing_hacks_js.ml
+++ lang_js/parsing/parsing_hacks_js.ml
@@ -1,6 +1,7 @@
 (* Yoann Padioleau
  *
  * Copyright (C) 2010, 2013 Facebook
+ * Copyright (C) 2019 Yoann Padioleau
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
