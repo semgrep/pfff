059ca2ca7ad850c3ad476e431a714230ebf888e9
pad <yoann.padioleau@gmail.com>
2019-07-12 10:46:24 +0200

    * lang_js/analyze/transpile_js.ml: transpiling patterns

--- lang_js/analyze/transpile_js.ml
+++ lang_js/analyze/transpile_js.ml
@@ -12,6 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
  * license.txt for more details.
  *)
+open Common
 
 module A = Ast_js
 module C = Cst_js
@@ -23,6 +24,7 @@ module C = Cst_js
  *
  * alt:
  *  - just call babel?
+ *  - r2s's transpiler (calls babel internally?)
  *
  * related:
  *  - babel and its "polyfill"
@@ -76,3 +78,58 @@ and xhp_body expr x =
 (*****************************************************************************)
 (* Patterns *)
 (*****************************************************************************)
+(* mostly a dupe of graph_code.ml, but avoid deps and take a tok *)
+let cnt = ref 0
+let gensym_name s tok =
+  incr cnt;
+  spf "!%s__%d!" s !cnt, tok
+
+let compile_pattern (_expr, fname, fpname) varname pat =
+  match pat with
+  | C.PatObj x ->
+    x |> C.unbrace |> C.uncomma |> List.map (fun pat ->
+      match pat with
+      (* { x, y } = varname; *)
+      | C.PatId (name, None) ->
+        let name = fname name in
+        let init = A.ObjAccess (A.Id (varname, ref A.NotResolved),
+                                A.PN name)
+        in
+        { A.v_name = name; v_kind = A.Let; v_init = init;
+          v_resolved = ref A.NotResolved;
+        }
+      (* { x: y, z } = varname; *)
+      | C.PatProp (pname, _tok, pat) ->
+        let pname = fpname pname in
+        (match pat with
+        | C.PatId (name, None) ->
+          let name = fname name in
+          let init = A.ObjAccess (A.Id (varname, ref A.NotResolved),
+                                  pname)
+          in
+          { A.v_name = name; v_kind = A.Let; v_init = init;
+            v_resolved = ref A.NotResolved;
+          }
+        | _ -> failwith "TODO: pattern not handled"
+        )
+      | _ -> failwith "TODO: pattern not handled"
+    )
+  | _ -> failwith "TODO: pattern not handled"
+        
+   
+
+let var_pattern (expr, fname, fpname) x =
+  match x.C.vpat_init with 
+  | None -> failwith "weird var_pattern without init; Part of ForOf?"
+  | Some (tok, e) ->
+    let e = expr e in
+    let vname, vars = 
+      match e with
+      | A.Id (name, _) -> name, []
+      | _ ->
+        let intermediate = gensym_name "tmp" tok in
+        let var = { A.v_name = intermediate; v_kind = A.Let; v_init = e;
+                    v_resolved = ref A.NotResolved } in
+        intermediate, [var]
+    in
+    vars @ compile_pattern (expr, fname, fpname) vname x.C.vpat
--- lang_js/analyze/transpile_js.mli
+++ lang_js/analyze/transpile_js.mli
@@ -2,3 +2,10 @@
 val xhp: 
   (Cst_js.expr -> Ast_js.expr) ->
   Cst_js.xhp_html -> Ast_js.expr
+
+val var_pattern:
+  ((Cst_js.expr -> Ast_js.expr) * 
+   (Cst_js.name -> Ast_js.name) *
+   (Cst_js.property_name -> Ast_js.property_name)
+  ) ->
+  Cst_js.variable_declaration_pattern -> Ast_js.var list
