dfe65462c956e7398f569a9efb644e36f6160622
pad <yoann.padioleau@gmail.com>
2019-10-27 17:54:10 +0100

    * lang_java/analyze/ast_java_simple.ml: deleted

--- lang_java/analyze/ast_java_simple.ml
+++
@@ -1,7 +0,0 @@
-(* 
- * The AST in lang_java/parsing/ is actually already a simple AST
- * as opposed to what we usually have in other language
- * directories (e.g. lang_php/). So we just have a simple
- * type alias here.
- *)
-type program = Ast_java.program
