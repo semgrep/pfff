95b885ccb4d7eac4e0e4ed889af24596719e265e
pad <yoann.padioleau@gmail.com>
2020-02-25 18:10:30 +0100

    * commons/file_type.ml: dyp

--- commons/file_type.ml
+++ commons/file_type.ml
@@ -93,7 +93,8 @@ let file_type_of_file2 file =
   match e with
 
   | "ml" | "mli" 
-  | "mly" | "mll"    
+  | "mly" | "mll"
+  | "dyp" (* dypgen =~ GLR ocamlyacc *)
       -> PL (ML e)
   | "mlb" (* mlburg *)
   | "mlp" (* used in some source *)
