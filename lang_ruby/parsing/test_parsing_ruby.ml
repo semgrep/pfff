2be0e4d6db4daafe82361e83336b2c60ca2c8435
pad <yoann.padioleau@gmail.com>
2020-02-25 17:23:49 +0100

    * lang_ruby/parsing/lib_parsing_ruby.ml: iterating over ruby files!

--- lang_ruby/parsing/test_parsing_ruby.ml
+++ lang_ruby/parsing/test_parsing_ruby.ml
@@ -1,5 +1,6 @@
 open Common
 
+module PI = Parse_info
 module Flag = Flag_parsing
 
 (*****************************************************************************)
@@ -19,28 +20,60 @@ let test_tokens file =
 let test_parse xs =
   let xs = List.map Common.fullpath xs in
 
-  let fullxs = xs
-(*
-    Lib_parsing_python.find_source_files_of_dir_or_files xs 
+  let fullxs = 
+    Lib_parsing_ruby.find_source_files_of_dir_or_files xs 
     |> Skip_code.filter_files_if_skip_list ~root:xs
-*)
   in
 
   let stat_list = ref [] in
+  let newscore  = Common2.empty_score () in
+  let ext = "rb" in
 
   fullxs |> Console.progress (fun k -> List.iter (fun file ->
     k();
 
     let (_xs, stat) =
+     let stat = Parse_info.default_stat file in
+     let n = Common2.nblines_file file in
      Common.save_excursion Flag.error_recovery true (fun () ->
      Common.save_excursion Flag.exn_when_lexical_error false (fun () ->
+      try
        let ast =  Parse_ruby.parse_program file in
-       ast, Parse_info.default_stat file
+       ast, { stat with PI.correct = n }
+      with exn ->
+        pr2 (Common.exn_to_s exn);
+        let stat = Parse_info.default_stat file in
+        [], { stat with PI.bad = n }
     )) in
     Common.push stat stat_list;
+    let s = spf "bad = %d" stat.PI.bad in
+    if stat.PI.bad = 0
+    then Hashtbl.add newscore file (Common2.Ok)
+    else Hashtbl.add newscore file (Common2.Pb s)
   ));
+  flush stdout; flush stderr;
+
   Parse_info.print_parsing_stat_list !stat_list;
+
+  (* todo: could factorize with other *)
+  let dirname_opt = 
+    match xs with
+    | [x] when Common2.is_directory x -> Some (Common.fullpath x)
+    | _ -> None
+  in
+  let score_path = Filename.concat Config_pfff.path "tmp" in
+  dirname_opt |> Common.do_option (fun dirname -> 
+    pr2 "--------------------------------";
+    pr2 "regression testing  information";
+    pr2 "--------------------------------";
+    let str = Str.global_replace (Str.regexp "/") "__" dirname in
+    Common2.regression_testing newscore 
+      (Filename.concat score_path
+       ("score_parsing__" ^str ^ ext ^ ".marshalled"))
+  );
+
   ()
+
 
 
 let test_dump file =
