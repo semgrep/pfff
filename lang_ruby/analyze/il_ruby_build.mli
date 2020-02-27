2f38ebab6f0d42c4a51a6dbd2c79b735e0c6b7f4
pad <yoann.padioleau@gmail.com>
2020-02-27 11:31:11 +0100

    * lang_ruby/parsing/parse_ruby.mli: cleanup interfaces

--- lang_ruby/analyze/il_ruby_build.mli
+++ lang_ruby/analyze/il_ruby_build.mli
@@ -1,35 +1,3 @@
 
 val refactor_ast : ?env:Utils_ruby.StrSet.t -> Ast_ruby.expr list -> Il_ruby.stmt
   (** Convert an Ast into a Cfg *)
-
-val re_init : unit -> unit
-  (** Re-initialize the global state of the module that is used to
-      provide unique identifier names.  Should only be necessary for
-      testing. *)
-
-val reparse : ?env:Utils_ruby.StrSet.t -> ?filename:string -> ?lineno:int -> 
-  (Il_ruby.stmt,'a) Il_ruby_printer.CodeUnparser.fmt -> 'a
-  (** This function takes an unparsing descript as described in
-      Cfg_printer and converts the unparsed string back into a
-      Cfg.stmt, allowing for easy tree rewriting as describedy by
-      Demaille, Levillain, and Sigoure's SAC'09 paper.  For example,
-
-      open Cfg_printer.CodeUnparser
-      reparse (s"x = " ++ expr) my_expr
-
-      returns the statement that assigns [my_expr] into the variable
-      [x].
-  *) 
-
-val kreparse : ?env:Utils_ruby.StrSet.t -> ?filename:string -> ?lineno:int -> 
-  (Il_ruby.stmt -> 'a) -> ('a,'b) Il_ruby_printer.CodeUnparser.fmt -> 'b
-  (** Similar to reparse, but passes the final stmt to the supplied
-      continuation *)
-
-val freparse : ?env:Utils_ruby.StrSet.t -> ?filename:string -> ?lineno:int -> 
-  ('a, Format.formatter, unit, Il_ruby.stmt) format4 -> 'a
-  (** Similar to reparse, but uses a format string instead *)
-
-val kfreparse : ?env:Utils_ruby.StrSet.t -> ?filename:string -> ?lineno:int -> 
-  (Il_ruby.stmt -> 'b) -> ('a, Format.formatter, unit, 'b) format4 -> 'a
-  (** Similar to kreparse, but uses a format string instead *)
--- lang_ruby/parsing/parse_ruby.ml
+++ lang_ruby/parsing/parse_ruby.ml
@@ -1,7 +1,24 @@
-module Ast = Ast_ruby
 module H = Ast_ruby_helpers
 module HH = Parser_ruby_helpers
 module Utils = Utils_ruby
+
+(*
+val set_lexbuf_fname : Lexing.lexbuf -> string -> unit
+val set_lexbuf_lineno : Lexing.lexbuf -> int -> unit
+val uniq_list : (Ast_ruby.expr list * 'a) list -> Ast_ruby.expr list list
+val parse_lexbuf_with_state :
+  ?env:Utils.StrSet.t -> Lexer_ruby.S.t -> Lexing.lexbuf -> Ast_ruby.ast
+val parse_string_with_state :
+  Lexer_ruby.S.t ->
+  ?env:Utils.StrSet.t ->
+  ?filename:string -> ?lineno:int -> string -> Ast_ruby.ast
+val parse_lexbuf : Lexing.lexbuf -> Ast_ruby.ast
+val parse_string :
+  ?env:Utils.StrSet.t ->
+  ?filename:string -> ?lineno:int -> string -> Ast_ruby.ast
+
+val parse_file : string -> Ast_ruby.ast
+*)
 
 let set_lexbuf_fname lexbuf name = 
   lexbuf.Lexing.lex_curr_p <-
@@ -59,7 +76,7 @@ let parse_lexbuf lexbuf =
   let state = Lexer_parser_ruby.create Lexer_ruby.top_lexer in 
     parse_lexbuf_with_state state lexbuf
 
-let parse_string ?env ?filename ?lineno str = 
+let _parse_string ?env ?filename ?lineno str = 
   let state = Lexer_parser_ruby.create Lexer_ruby.top_lexer in 
     parse_string_with_state state ?env ?filename ?lineno str
 
