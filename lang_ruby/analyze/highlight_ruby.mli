c003eac41ea85f199393ecf8780f632ad46b9a45
pad <yoann.padioleau@gmail.com>
2020-03-09 15:03:02 +0100

    * Makefile: install ruby libs for codemap

--- lang_ruby/analyze/highlight_ruby.mli
+++ lang_ruby/analyze/highlight_ruby.mli
@@ -2,5 +2,5 @@
 val visit_program :
   tag_hook: (Parse_info.t -> Highlight_code.category -> unit) ->
   Highlight_code.highlighter_preferences ->
-  Ast_ruby.program * Parser_ruby.token list ->
+  Ast_ruby.program option * Parser_ruby.token list ->
   unit
