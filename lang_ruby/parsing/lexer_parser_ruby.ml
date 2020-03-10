47af309bfba4c05eef8cf83ca3c4bb68c79924a9
pad <yoann.padioleau@gmail.com>
2020-03-10 10:07:17 +0100

    * lang_ruby/parsing/lexer_parser_ruby.ml: cleanup, rename constructors

--- lang_ruby/parsing/lexer_parser_ruby.ml
+++ lang_ruby/parsing/lexer_parser_ruby.ml
@@ -1,34 +1,27 @@
-type expr_state =     
-  | Expr_Beg (* beginning of line *)
-  | Expr_Mid (* after command, before args *)
-  | Expr_End (* end of expr / literal *) 
-  | Expr_Def (* after a 'def' or '.' token *)
-  | Expr_Local (* after a local variable *)
+type state =     
+  | Bol (* beginning of line *)
+  | AfterCommand (* after command, before args *)
+  | EndOfExpr (* end of expr / literal *) 
+  | AfterDef (* after a 'def' or '.' token *)
+  | AfterLocal (* after a local variable *)
 
 type t = { 
-  mutable expr_state : expr_state;
+  mutable state : state;
   lexer_stack : cps_lexer Stack.t;
 }
 
 and cps_lexer = t -> Lexing.lexbuf -> Parser_ruby.token
 
-let beg_state t = t.expr_state <- Expr_Beg
-let mid_state t = t.expr_state <- Expr_Mid
-let end_state t = t.expr_state <- Expr_End
-let def_state t = t.expr_state <- Expr_Def
-let local_state t = t.expr_state <- Expr_Local
+let beg_state t = t.state <- Bol
+let mid_state t = t.state <- AfterCommand
+let end_state t = t.state <- EndOfExpr
+let def_state t = t.state <- AfterDef
+let local_state t = t.state <- AfterLocal
 
 let create entry = 
   let stk = Stack.create () in
     Stack.push entry stk;
-    {expr_state = Expr_Beg;
+    {state = Bol;
      lexer_stack = stk;
     }
-
-let to_string = function
-  | Expr_Beg -> "Expr_Beg"
-  | Expr_Mid -> "Expr_Mid"
-  | Expr_End -> "Expr_End"
-  | Expr_Def -> "Expr_Def"
-  | Expr_Local -> "Expr_Local"
 
