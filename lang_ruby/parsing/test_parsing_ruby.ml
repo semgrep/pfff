20fee529d700cc762556ce7dd2220ab9ec30bc5e
pad <yoann.padioleau@gmail.com>
2020-03-15 11:29:17 +0100

    * lang_ruby/parsing/test_parsing_ruby.ml: no fullinfo dump

--- lang_ruby/parsing/test_parsing_ruby.ml
+++ lang_ruby/parsing/test_parsing_ruby.ml
@@ -93,10 +93,12 @@ let test_parse xs =
 
 
 let test_dump file =
+  if false then begin
   Meta_parse_info._current_precision := { 
     Meta_parse_info.default_dumper_precision with
     Meta_parse_info.full_info = true;
   };
+  end;
   let ast = Parse_ruby.parse_program file in
   let v = Meta_ast_ruby.vof_program ast in
   let s = Ocaml.string_of_v v in
