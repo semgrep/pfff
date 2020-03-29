ae334dd182f24847da5e09bf5f9c0c2a3bfe9281
pad <yoann.padioleau@gmail.com>
2020-03-29 11:02:05 +0200

    * lang_ml/parsing/parser_ml.mly: switch to menhir

--- scripts/yacc_to_menhir.pl
+++ scripts/yacc_to_menhir.pl
@@ -1,36 +1,72 @@
 #!/usr/bin/perl
 
+my $after_percentpercent = 0;
+
 while(<>) {
+    if(/^%%/) { $after_percentpercent = 1; }
+
   # remove C-style comments
-  s#/\*##g;
-  s#\*/##g;
+  s#/\*\(\*#(*#g;
+  s#\*\)\*/#*)#g;
+  s#/\*#(*#g;
+  s#\*/#*)#g;
 
   # remove pad sectioning number
   s#\(\*[0-9] #(* #g;
+
+    if($after_percentpercent) {
   
   # use chars instead of long tokens for parens-like tokens
-  s#\bT_LPAREN\b#"("#g;
-  s#\bT_RPAREN\b#")"#g;
+  s#\bTOParen\b#"("#g;
+  s#\bTCParen\b#")"#g;
 
-  s#\bT_LBRACKET\b#"["#g;
-  s#\bT_RBRACKET\b#"]"#g;
+  s#\bTOBracket\b#"["#g;
+  s#\bTCBracket\b#"]"#g;
 
-  s#\bT_LCURLY\b#"{"#g;
-  s#\bT_RCURLY\b#"}"#g;
+  s#\bTOBracketLess\b#"[<"#g;
+  s#\bTGreaterCBracket\b#">]"#g;
+  s#\bTOBracketPipe\b#"[|"#g;
+  s#\bTPipeCBracket\b#"|]"#g;
+  s#\bTOBraceLess\b#"{<"#g;
+  s#\bTGreaterCBrace\b#">}"#g;
+
+  s#\bTOBrace\b#"{"#g;
+  s#\bTCBrace\b#"}"#g;
 
   # use chars instead of long tokens for common punctuators
-  s#\bT_SEMICOLON\b#";"#g;
-  s#\bT_COMMA\b#","#g;
-  s#\bT_PERIOD\b#"."#g;
-  s#\bT_COLON\b#":"#g;
+  s#\bTSemiColon\b#";"#g;
+  s#\bTComma\b#","#g;
+  s#\bTDot\b#"."#g;
+  s#\bTColon\b#":"#g;
 
-  s#\bT_DOTS\b#"..."#g;
+  s#\bTSemiColonSemiColon\b#";;"#g;
+  s#\bTDotDot\b#".."#g;
+  s#\bTColonColon\b#"::"#g;
+  s#\bTQuestionQuestion\b#"??"#g;
+
+
+  s#\bTQuestion\b#"?"#g;
+  s#\bTTilde\b#"~"#g;
+  s/\bTSharp\b/"#"/g;
+  s#\bTColonGreater\b#":>"#g;
+
+  s#\bTPipe\b#"|"#g;
+  s#\bTAssignMutable\b#"<-"#g;
+  s#\bTAssign\b#":="#g;
+  s#\bTBang\b#"!"#g;
+  s#\bTArrow\b#"->"#g;
+  s#\bTUnderscore\b#"_"#g;
+
+#  s#\bT_DOTS\b#"..."#g;
 
   # use chars instead of long tokens for important operators
-  s#\bT_MULT\b#"*"#g;
-  s#\bT_ASSIGN\b#"="#g;
+  s#\bTStar\b#"*"#g;
+  s#\bTEq\b#"="#g;
 
-  
+  s#\bTQuote\b#"'"#g;
+  s#\bTBackQuote\b#"`"#g;
+
+  }  
 
   print;
 }
