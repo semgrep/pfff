035a36f464ca38cb3ba056ee836e6d89c0bc5b68
pad <yoann.padioleau@gmail.com>
2020-04-03 17:05:09 +0200

    * lang_GENERIC/analyze/naming_ast.ml: switch to multiple scopes (global, blocks, imported) so can at least not lookup in global scope when inside a function in Python

--- tests/python/naming/scope.py
+++ tests/python/naming/scope.py
@@ -1,11 +1,18 @@
+import foo
+from foo import imported
+
 global_ = "global"
 
 def foo(param1, param2):
   local = "local"
   # actually a new local!
   global_ = "local"
+  # actually not a new local but imported one?
+  imported = "imported"
   return local
   return param1
+  return imported
+  
 
 def foo2():
   global global_
@@ -19,3 +26,10 @@ def foo3(x, y):
      nonlocal y
      return x+y
    yield bar(x)
+
+print(foo.imported)
+print(global_)
+foo(1,2)
+print(global_)
+foo2()
+print(global_)
