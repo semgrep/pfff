9a814c562d5d0df695e5bef93cd3b9bff4ffb187
Nathan Brahms <nathan@returntocorp.com>
2020-05-06 15:13:49 -0700

    Correctly parse tuples inside Python f-strings
    Previously, f-strings could only accept expressions that could also
    appear as Python statements. However, Python automatically converts
    comma-separated lists in f-string interpolants into tuples.
    
    This commit adds that behavior to pfff's Python parser as well.

--- tests/python/parsing/fstrings.py
+++ tests/python/parsing/fstrings.py
@@ -12,6 +12,9 @@ def test():
    foo(f"this is {1+2 if True else 4}")
    foo(f'this can contain double quotes " {1+2 if True else 4}')
    foo(f"this is {{ not: an expr}}")
+   foo(f"this is a {tuple,}")
+   foo(f"this is also {a, tuple}")
+   foo(f"this is {also, a + tuple,}")
    echo_error(f"Error while running {tool_id}: {findings}")
    # what is that?
    echo_warning(f"{len(collapsed_findings)} findings in {elapsed:.2f} s\n")
