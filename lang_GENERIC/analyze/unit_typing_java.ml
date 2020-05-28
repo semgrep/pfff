bbf5425c938ab97a595d98ac936b0f5450790742
Emma Jin <emjin@users.noreply.github.com>
2020-05-28 16:43:21 -0400

    Added test using basic parameter passing.
    In unit_typing_java, added test to check that parameter types are propagated correctly. Verified by running make test

--- lang_GENERIC/analyze/unit_typing_java.ml
+++ lang_GENERIC/analyze/unit_typing_java.ml
@@ -67,4 +67,34 @@ let unittest =
           assert_failure (spf "it should correctly parse %s" file)
     );
 
+    "test basic params" >:: (fun () ->
+      let file = Filename.concat Config_pfff.path "/tests/GENERIC/typing/BasicParam.java" in
+        try
+          let ast = Parse_generic.parse_program file in
+            let lang = List.hd (Lang.langs_of_filename file) in 
+             Naming_AST.resolve lang ast;
+
+            let v = V.mk_visitor { V.default_visitor with
+                V.kexpr = (fun (_k, _) exp ->
+                  match exp with
+                    | A.Call(_, x::y::[]) ->
+                      ((match x with
+                        | A.Arg(A.Id(_, {A.id_type=id_type; _})) -> (
+                              match !id_type with 
+                                | Some(A.TyBuiltin((("int", _)))) -> ()
+                                | _ -> assert_failure("Variable 1 referenced did not have expected type String"))
+                        | _ -> ());
+                      (match y with
+                        | A.Arg(A.Id(_, {A.id_type=id_type; _})) -> (
+                              match !id_type with 
+                                | Some(A.TyBuiltin((("boolean", _)))) -> ()
+                                | _ -> assert_failure("Variable 2 referenced did not have expected type int"))
+                        | _ -> ()))
+                    | _ -> ()
+                );
+            } in
+            v (A.Pr ast)
+        with Parse_info.Parsing_error _ ->
+          assert_failure (spf "it should correctly parse %s" file)
+    );
   ]
