107c619a20f6a3a86db7af420e17eba994de06bf
Emma Jin <emjin@users.noreply.github.com>
2020-06-04 17:40:52 -0400

    Added a test case for parsing
    Checks whether the following parse successfully (does not check that their types are populated)
    
    $T $B;
    ...
    (Set<String> $B);
    ...
    (String $B).func();
    ...
    func ((typ $A));
    ...
    $A = (typ $B);
    ...
    if ($X == (String $Y)) { ... }
    ...
    (Bar $A).$F = $FUNC((String $B), ...);
    
    Tested: make test in pfff

--- lang_GENERIC/analyze/Unit_typing_generic.ml
+++ lang_GENERIC/analyze/Unit_typing_generic.ml
@@ -97,4 +97,16 @@ let unittest =
         with Parse_info.Parsing_error _ ->
           assert_failure (spf "it should correctly parse %s" file)
     );
+
+    "pattern_files" >:: (fun () ->
+      let dir = Filename.concat Config_pfff.path "/tests/java/semgrep" in
+      let files = Common2.glob (spf "%s/*.sgrep" dir) in
+      files |> List.iter (fun file ->
+        try
+          let _ = Parse_generic.parse_pattern Lang.Java (Common.read_file file) in
+          ()
+        with Parse_info.Parsing_error _ ->
+          assert_failure (spf "it should correctly parse %s" file)
+      )
+    );
   ]
