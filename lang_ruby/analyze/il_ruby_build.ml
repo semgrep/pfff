563859b4ee8b7609f20011624267166fae9b099a
pad <yoann.padioleau@gmail.com>
2020-06-29 15:06:10 +0200

    * lang_ruby/parsing/ast_ruby.ml: progress

--- lang_ruby/analyze/il_ruby_build.ml
+++ lang_ruby/analyze/il_ruby_build.ml
@@ -524,7 +524,8 @@ let rec refactor_expr (acc:stmt acc) (e : Ast.expr) : stmt acc * Il_ruby.expr =
     | Ast.S Ast.Case _ | Ast.S Ast.ExnBlock _ 
     | Ast.D Ast.EndBlock _ | Ast.D Ast.BeginBlock _  
     | Ast.CodeBlock _ | Ast.D Ast.MethodDef _ 
-    | Ast.S Ast.For _ | Ast.S Ast.Unless _ | Ast.S Ast.Until _
+    | Ast.S Ast.For _ | Ast.S Ast.For2 _
+    | Ast.S Ast.Unless _ | Ast.S Ast.Until _
     | Ast.S Ast.While _ 
     | Ast.Ternary _ | Ast.D Ast.Alias _ | Ast.D Ast.Undef _ as s ->
         (* These cases are where stmts are embedded in expressions,
@@ -1505,6 +1506,8 @@ and refactor_stmt (acc: stmt acc) (e:Ast.expr) : stmt acc =
 
   | Ast.S Ast.Until(pos, b,g,body) -> 
       refactor_stmt acc (Ast.S (Ast.While(pos, b,Ast.Unary((Ast.Op_UNot,pos),g),body)))
+
+  | Ast.S Ast.For2 _ -> failwith "TODO"
 
   | Ast.S Ast.For(pos, formals,guard,body) ->
       let acc, formals = refactor_block_formal_list acc formals pos in
