semgrep/pfff

commit 22f246807c64adda48f50f555f0fcf8914afbc78

generic: constness: Handle overflows and division by zero (#373)

lang_GENERIC/analyze/Dataflow_constness.ml
@@ -155,13 +155,42 @@ let eval_unop_int op opt_i =
   | G.Minus, Some i -> G.Lit (literal_of_int (-i))
   | ___else____     -> G.Cst G.Cint
 
+(* This reduces arithmetic "exceptions" to `G.Cst G.Cint`, it does NOT
+ * detect overflows for any other language than OCaml. Note that OCaml
+ * integers have just 63-bits in 64-bit architectures!
+*)
 let eval_binop_int op opt_i1 opt_i2 =
+  let sign i = i asr (Sys.int_size-1) in
   match op, opt_i1, opt_i2 with
-  (* TODO: Handle overflows and division by zero. *)
-  | G.Plus,  Some i1, Some i2 -> G.Lit (literal_of_int (i1 + i2))
-  | G.Minus, Some i1, Some i2 -> G.Lit (literal_of_int (i1 - i2))
-  | G.Mult,  Some i1, Some i2 -> G.Lit (literal_of_int (i1 * i2))
-  | G.Div,   Some i1, Some i2 -> G.Lit (literal_of_int (i1 / i2))
+  | G.Plus,  Some i1, Some i2 ->
+    let r = i1 + i2 in
+    if sign i1 = sign i2 && sign r <> sign i1 then
+      G.Cst G.Cint (* overflow *)
+    else
+      G.Lit (literal_of_int (i1 + i2))
+  | G.Minus, Some i1, Some i2 ->
+    let r = i1 - i2 in
+    if sign i1 <> sign i2 && sign r <> sign i1 then
+      G.Cst G.Cint (* overflow *)
+    else
+      G.Lit (literal_of_int (i1 + i2))
+  | G.Mult,  Some i1, Some i2 ->
+    (* Who knows, one day we may be running in 128-bit architectures... *)
+    if Sys.int_size < 64 then
+      let r = Int64.mul (Int64.of_int i1) (Int64.of_int i2) in
+      if Int64.compare (Int64.of_int min_int) r <= 0
+          && Int64.compare r (Int64.of_int max_int) <= 0 then
+        G.Lit (literal_of_int (Int64.to_int r))
+      else
+        G.Cst G.Cint (* overflow *)
+    else
+      G.Cst G.Cint
+  | G.Div,   Some i1, Some i2 ->
+    if i1 = min_int && i2 = -1 then
+      G.Cst G.Cint (* = max_int+1, overflow *)
+    else
+      (try G.Lit (literal_of_int (i1 / i2))
+      with Division_by_zero -> G.Cst G.Cint)
   | ___else____     -> G.Cst G.Cint
 
 let eval_binop_string op s1 s2 =

tests/python/dataflow/big-mult.py
@@ -1,0 +1,4 @@
+def foo():
+    big = 4611686018427387903 * 4611686018427387903
+    return big
+

tests/python/dataflow/div-by-zero.py
@@ -1,0 +1,4 @@
+def foo():
+    bad = 1/0
+    return bad
+

tests/python/dataflow/mult.py
@@ -1,0 +1,4 @@
+def foo():
+    x = 42 * 42
+    return x
+

commit 8a3a602c1716a8752b088bbad285d970f934aed1

generic: constness: Fix overflow check for multiplication
To do it that way one needs 128-bit integers, but those are not
available in OCaml's stdlib.

Also fix OCaml indentation with ocp-indent.

Fixes: 22f246807 ("generic: constness: Handle overflows and division by zero (#373)")

lang_GENERIC/analyze/Dataflow_constness.ml
@@ -163,34 +163,38 @@ let eval_binop_int op opt_i1 opt_i2 =
   let sign i = i asr (Sys.int_size-1) in
   match op, opt_i1, opt_i2 with
   | G.Plus,  Some i1, Some i2 ->
-    let r = i1 + i2 in
-    if sign i1 = sign i2 && sign r <> sign i1 then
-      G.Cst G.Cint (* overflow *)
-    else
-      G.Lit (literal_of_int (i1 + i2))
-  | G.Minus, Some i1, Some i2 ->
-    let r = i1 - i2 in
-    if sign i1 <> sign i2 && sign r <> sign i1 then
-      G.Cst G.Cint (* overflow *)
-    else
-      G.Lit (literal_of_int (i1 + i2))
-  | G.Mult,  Some i1, Some i2 ->
-    (* Who knows, one day we may be running in 128-bit architectures... *)
-    if Sys.int_size < 64 then
-      let r = Int64.mul (Int64.of_int i1) (Int64.of_int i2) in
-      if Int64.compare (Int64.of_int min_int) r <= 0
-          && Int64.compare r (Int64.of_int max_int) <= 0 then
-        G.Lit (literal_of_int (Int64.to_int r))
-      else
+      let r = i1 + i2 in
+      if sign i1 = sign i2 && sign r <> sign i1 then
         G.Cst G.Cint (* overflow *)
-    else
-      G.Cst G.Cint
+      else
+        G.Lit (literal_of_int (i1 + i2))
+  | G.Minus, Some i1, Some i2 ->
+      let r = i1 - i2 in
+      if sign i1 <> sign i2 && sign r <> sign i1 then
+        G.Cst G.Cint (* overflow *)
+      else
+        G.Lit (literal_of_int (i1 + i2))
+  | G.Mult,  Some i1, Some i2 ->
+      let overflow =
+        i1 <> 0 && i2 <> 0 &&
+        ((i1 < 0 && i2 = min_int)       (* >max_int *)
+         || (i1 = min_int && i2 < 0)    (* >max_int *)
+         || (if sign i1 * sign i2 = 1 then
+               abs i1 > abs(max_int/i2) (* >max_int *)
+             else
+               abs i1 > abs(min_int/i2) (* <min_int *)
+            ))
+      in
+      if overflow then
+        G.Cst G.Cint
+      else
+        G.Lit (literal_of_int (i1 * i2))
   | G.Div,   Some i1, Some i2 ->
-    if i1 = min_int && i2 = -1 then
-      G.Cst G.Cint (* = max_int+1, overflow *)
-    else
-      (try G.Lit (literal_of_int (i1 / i2))
-      with Division_by_zero -> G.Cst G.Cint)
+      if i1 = min_int && i2 = -1 then
+        G.Cst G.Cint (* = max_int+1, overflow *)
+      else
+        (try G.Lit (literal_of_int (i1 / i2))
+         with Division_by_zero -> G.Cst G.Cint)
   | ___else____     -> G.Cst G.Cint
 
 let eval_binop_string op s1 s2 =

tests/python/dataflow/big-mult.py
@@ -1,4 +1,3 @@
 def foo():
     big = 4611686018427387903 * 4611686018427387903
     return big
-

tests/python/dataflow/div-by-zero.py
@@ -1,4 +1,3 @@
 def foo():
     bad = 1/0
     return bad
-

tests/python/dataflow/mult.py
@@ -1,4 +1,3 @@
 def foo():
     x = 42 * 42
     return x
-