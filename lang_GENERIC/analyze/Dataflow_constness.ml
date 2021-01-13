semgrep/pfff

commit 22f246807c64adda48f50f555f0fcf8914afbc78

generic: constness: Handle overflows and division by zero (#373)

lang_GENERIC/analyze/Dataflow_constness.ml
@@ -155,13 +155,42 @@ let eval_unop_int op opt_i =
   | G.Minus, Some i -> G.Lit (literal_of_int (-i))
   | ___else____     -> G.Cst G.Cint
 
+(* This reduces arithmetic "exceptions" to `G.Cst G.Cint`, it does NOT
+ * detect overflows for any other language than OCaml. Note that OCaml
+ * integers have just 63-bits in 64-bit architectures!
+*)
 let eval_binop_int op opt_i1 opt_i2 =
+  let sign i = i asr (Sys.int_size-1) in
   match op, opt_i1, opt_i2 with
-  (* TODO: Handle overflows and division by zero. *)
-  | G.Plus,  Some i1, Some i2 -> G.Lit (literal_of_int (i1 + i2))
-  | G.Minus, Some i1, Some i2 -> G.Lit (literal_of_int (i1 - i2))
-  | G.Mult,  Some i1, Some i2 -> G.Lit (literal_of_int (i1 * i2))
-  | G.Div,   Some i1, Some i2 -> G.Lit (literal_of_int (i1 / i2))
+  | G.Plus,  Some i1, Some i2 ->
+    let r = i1 + i2 in
+    if sign i1 = sign i2 && sign r <> sign i1 then
+      G.Cst G.Cint (* overflow *)
+    else
+      G.Lit (literal_of_int (i1 + i2))
+  | G.Minus, Some i1, Some i2 ->
+    let r = i1 - i2 in
+    if sign i1 <> sign i2 && sign r <> sign i1 then
+      G.Cst G.Cint (* overflow *)
+    else
+      G.Lit (literal_of_int (i1 + i2))
+  | G.Mult,  Some i1, Some i2 ->
+    (* Who knows, one day we may be running in 128-bit architectures... *)
+    if Sys.int_size < 64 then
+      let r = Int64.mul (Int64.of_int i1) (Int64.of_int i2) in
+      if Int64.compare (Int64.of_int min_int) r <= 0
+          && Int64.compare r (Int64.of_int max_int) <= 0 then
+        G.Lit (literal_of_int (Int64.to_int r))
+      else
+        G.Cst G.Cint (* overflow *)
+    else
+      G.Cst G.Cint
+  | G.Div,   Some i1, Some i2 ->
+    if i1 = min_int && i2 = -1 then
+      G.Cst G.Cint (* = max_int+1, overflow *)
+    else
+      (try G.Lit (literal_of_int (i1 / i2))
+      with Division_by_zero -> G.Cst G.Cint)
   | ___else____     -> G.Cst G.Cint
 
 let eval_binop_string op s1 s2 =

tests/python/dataflow/big-mult.py
@@ -1,0 +1,4 @@
+def foo():
+    big = 4611686018427387903 * 4611686018427387903
+    return big
+

tests/python/dataflow/div-by-zero.py
@@ -1,0 +1,4 @@
+def foo():
+    bad = 1/0
+    return bad
+

tests/python/dataflow/mult.py
@@ -1,0 +1,4 @@
+def foo():
+    x = 42 * 42
+    return x
+

commit 57accd55051c50805a4cbd2a8a4d9a9558d442f9

Fix overflow check for multiplication and warn on division by zero (#377)
* generic: constness: Fix overflow check for multiplication

To do it that way one needs 128-bit integers, but those are not
available in OCaml's stdlib.

Also fix OCaml indentation with ocp-indent.

Fixes: 22f246807 ("generic: constness: Handle overflows and division by zero (#373)")

* generic: constness: Warn when we encounter a division by zero

Co-authored-by: Yoann Padioleau <pad@returntocorp.com>

lang_GENERIC/analyze/Dataflow_constness.ml
@@ -36,6 +36,19 @@ module DataflowX = Dataflow.Make (struct
   end)
 
 (*****************************************************************************)
+(* Error management *)
+(*****************************************************************************)
+
+let warning tok s =
+  let opt_loc =
+    try Some (Parse_info.string_of_info tok)
+    with Parse_info.NoTokenLocation _ -> None
+  in
+  match opt_loc with
+  | Some loc -> pr2 (spf "%s: %s" loc s)
+  | None     -> pr2 s
+
+(*****************************************************************************)
 (* Helpers *)
 (*****************************************************************************)
 
@@ -159,7 +172,7 @@ let eval_unop_int op opt_i =
  * detect overflows for any other language than OCaml. Note that OCaml
  * integers have just 63-bits in 64-bit architectures!
 *)
-let eval_binop_int op opt_i1 opt_i2 =
+let eval_binop_int tok op opt_i1 opt_i2 =
   let sign i = i asr (Sys.int_size-1) in
   match op, opt_i1, opt_i2 with
   | G.Plus,  Some i1, Some i2 ->
@@ -175,22 +188,28 @@ let eval_binop_int op opt_i1 opt_i2 =
       else
         G.Lit (literal_of_int (i1 + i2))
   | G.Mult,  Some i1, Some i2 ->
-      (* Who knows, one day we may be running in 128-bit architectures... *)
-      if Sys.int_size < 64 then
-        let r = Int64.mul (Int64.of_int i1) (Int64.of_int i2) in
-        if Int64.compare (Int64.of_int min_int) r <= 0
-        && Int64.compare r (Int64.of_int max_int) <= 0 then
-          G.Lit (literal_of_int (Int64.to_int r))
-        else
-          G.Cst G.Cint (* overflow *)
-      else
+      let overflow =
+        i1 <> 0 && i2 <> 0 &&
+        ((i1 < 0 && i2 = min_int)       (* >max_int *)
+         || (i1 = min_int && i2 < 0)    (* >max_int *)
+         || (if sign i1 * sign i2 = 1 then
+               abs i1 > abs(max_int/i2) (* >max_int *)
+             else
+               abs i1 > abs(min_int/i2) (* <min_int *)
+            ))
+      in
+      if overflow then
         G.Cst G.Cint
+      else
+        G.Lit (literal_of_int (i1 * i2))
   | G.Div,   Some i1, Some i2 ->
       if i1 = min_int && i2 = -1 then
         G.Cst G.Cint (* = max_int+1, overflow *)
       else
         (try G.Lit (literal_of_int (i1 / i2))
-         with Division_by_zero -> G.Cst G.Cint)
+         with Division_by_zero ->
+           warning tok "Found division by zero";
+           G.Cst G.Cint)
   | ___else____     -> G.Cst G.Cint
 
 let eval_binop_string op s1 s2 =
@@ -223,16 +242,17 @@ and eval_lval env lval =
   | ___else___ ->
       G.NotCst
 
-and eval_op env op args =
+and eval_op env wop args =
+  let op, tok = wop in
   let cs = List.map (eval env) args in
-  match fst op, cs with
+  match op, cs with
   | G.Plus, [c1]             -> c1
   | op,     [G.Lit (G.Bool (b, _))] -> eval_unop_bool op b
   | op,     [G.Lit (G.Int _ as li)] -> eval_unop_int op (int_of_literal li)
   | op,     [G.Lit (G.Bool (b1, _)); G.Lit (G.Bool (b2, _))] ->
       eval_binop_bool op b1 b2
   | op,     [G.Lit (G.Int _ as li1); G.Lit (G.Int _ as li2)] ->
-      eval_binop_int op (int_of_literal li1) (int_of_literal li2)
+      eval_binop_int tok op (int_of_literal li1) (int_of_literal li2)
   | op,     [G.Lit (G.String (s1, _)); G.Lit (G.String (s2, _))] ->
       eval_binop_string op s1 s2
   | _op,    [G.Cst _ as c1]    -> c1