a594cfed76615bc18ea83e9bafabaea78deab07e
Martin Jambon <martin@mjambon.com>
2021-01-19 18:59:45 -0800

    Add hack to have two sets of equality functions

--- lang_GENERIC_base/AST_generic.ml
+++ lang_GENERIC_base/AST_generic.ml
@@ -147,6 +147,21 @@ open Ppx_hash_lib.Std.Hash.Builtin
 (* Accessories *)
 (*****************************************************************************)
 
+(* A set of metavariables. Access cost is O(log n). *)
+module String_set = struct
+  type t = string Set_.t
+
+  type string_list = string list
+  [@@deriving show]
+
+  let pp fmt x =
+    pp_string_list fmt (Set_.elements x)
+end
+
+(*****************************************************************************)
+(* Comparison and hashing *)
+(*****************************************************************************)
+
 (*
    Create a node ID that is guaranteed to be unique within an AST,
    as long as the whole AST is created within the same process.
@@ -177,16 +192,72 @@ end
 (* ppx_hash refuses to hash mutable fields but we do it anyway. *)
 let hash_fold_ref hash_fold_x acc x = hash_fold_x acc !x
 
-(* A set of metavariables. Access cost is O(log n). *)
-module String_set = struct
-  type t = string Set_.t
+(*
+   Trickery to offer two collections of equality functions:
 
-  type string_list = string list
-  [@@deriving show]
+   - structural equality: do two AST nodes have the same structure?
+     This diregards IDs assigned uniquely to AST nodes.
+   - referential equality: are these two AST nodes physically the same?
+     This is essentially physical equality but it tolerates copying or even
+     some transformation as long as the node ID is preserved.
 
-  let pp fmt x =
-    pp_string_list fmt (Set_.elements x)
-end
+   Comparing two AST nodes must be done via one of the with_equal_* wrappers
+   so as to select structural or referential equality.
+*)
+
+type busy_with_equal = Not_busy | Structural_equal | Referential_equal
+
+(* global state! managed by the with_equal_* functions *)
+let busy_with_equal = ref Not_busy
+
+let equal_stmt_field_s equal_stmt_kind a b =
+  match !busy_with_equal with
+  | Not_busy ->
+      (* Call 'Structural.equal_XXX or 'Referential.equal_XXX' to avoid this
+         error. *)
+      assert false
+  | Structural_equal -> equal_stmt_kind a b
+  | Referential_equal -> true
+
+let equal_stmt_field_s_id a b =
+  match !busy_with_equal with
+  | Not_busy ->
+      (* Call 'Structural.equal_XXX or 'Referential.equal_XXX' to avoid this
+         error. *)
+      assert false
+  | Structural_equal -> true
+  | Referential_equal -> Node_ID.equal a b
+
+(*
+   Wrap one of the generated equal_* functions into one that selects
+   structural equality, ignoring node IDs and position information.
+*)
+let with_structural_equal equal a b =
+  match !busy_with_equal with
+  | Not_busy ->
+      busy_with_equal := Structural_equal;
+      Fun.protect
+        ~finally:(fun () -> busy_with_equal := Not_busy)
+        (fun () -> equal a b)
+  | Structural_equal
+  | Referential_equal ->
+      failwith "an equal is already in progress"
+
+(*
+   Wrap one of the generated equal_* functions into one that selects
+   referential equality, using node IDs when available.
+*)
+let with_referential_equal equal a b =
+  match !busy_with_equal with
+  | Not_busy ->
+      busy_with_equal := Referential_equal;
+      Fun.protect
+        ~finally:(fun () -> busy_with_equal := Not_busy)
+        (fun () -> equal a b)
+  | Structural_equal
+  | Referential_equal ->
+      failwith "an equal is already in progress"
+
 
 (*****************************************************************************)
 (* Token (leaf) *)
@@ -787,18 +858,16 @@ and other_expr_operator =
 (*****************************************************************************)
 (* Statement *)
 (*****************************************************************************)
-(*
-   Two statements are considered equal iff their ID ('s_id' field) is the
-   same. The 'equal_stmt' and 'hash_stmt' functions work accordingly.
-*)
 (*s: type [[AST_generic.stmt]] *)
 and stmt = {
-  s: stmt_kind [@equal fun _a _b -> true] [@hash.ignore];
+  s: stmt_kind
+     [@equal (equal_stmt_field_s equal_stmt_kind)]
+     [@hash.ignore];
 
   (* this can be used to compare and hash more efficiently stmts,
      or in semgrep to quickly know if a stmt is a children of another stmt.
   *)
-  s_id: Node_ID.t;
+  s_id: Node_ID.t [@equal equal_stmt_field_s_id];
 
   (* todo? we could store a range: (tok * tok) to delimit the range of a stmt
    * which would allow us to remove some of the extra 'tok' in stmt_kind.
