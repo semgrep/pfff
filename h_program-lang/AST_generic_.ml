b7a8ec62ee35493e325b822861feb3e58fb594bd
pad <yoann.padioleau@gmail.com>
2021-05-26 14:36:21 +0200

    is_metavar_ellipsis

--- h_program-lang/AST_generic_.ml
+++ h_program-lang/AST_generic_.ml
@@ -59,9 +59,14 @@ type function_kind =
  * Metavars_generic.is_metavar_name. If you use the function below,
  * it probably means you have an ugly dependency to semgrep that you
  * should not have.
+ * coupling: Metavariable.is_metavar_name
 *)
 let is_metavar_name s =
   s =~ "^\\(\\$[A-Z_][A-Z_0-9]*\\)$"
+(* coupling: Metavariable.is_metavar_ellipsis *)
+let is_metavar_ellipsis s =
+  s =~ "^\\(\\$\\.\\.\\.[A-Z_][A-Z_0-9]*\\)$"
+
 
 (* ugly! See AST_generic.ml for more information *)
 let special_multivardef_pattern = "!MultiVarDef!"
