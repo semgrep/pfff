semgrep/pfff

commit 913836e07f55042f3469ad9d46fad546e8fdf6e3

parse_info: Add function to adjust token_mutable wrt a base location

h_program-lang/Parse_info.ml
@@ -694,6 +694,28 @@ let mk_lexer_for_yacc toks is_comment =
   let lexbuf_fake = Lexing.from_function (fun _buf _n -> raise Impossible) in
   tr, lexer, lexbuf_fake
 
+let adjust_pinfo_wrt_base base_loc loc =
+  { loc with
+    charpos = base_loc.charpos + loc.charpos - 1;
+    line    = base_loc.line + loc.line - 1;
+    column  =
+      if loc.line = 1 then
+        base_loc.column + loc.column - 1
+      else
+        loc.column;
+    file    = base_loc.file; }
+
+let adjust_info_wrt_base base_loc ii =
+  { ii with token =
+              match ii.token with
+              | OriginTok pi ->
+                  OriginTok(adjust_pinfo_wrt_base base_loc pi)
+              | ExpandedTok (pi, vpi, off) ->
+                  ExpandedTok(adjust_pinfo_wrt_base base_loc pi, vpi, off)
+              | FakeTokStr (s, vpi_opt) ->
+                  FakeTokStr (s, vpi_opt)
+              | Ab -> Ab
+  }
 
 (*****************************************************************************)
 (* Error location report *)

h_program-lang/Parse_info.mli
@@ -303,6 +303,13 @@ val complete_token_location_large :
   Common.filename -> (int -> (int * int))  -> token_location -> token_location
 (*e: signature [[Parse_info.complete_token_location_large]] *)
 
+(** [adjust_info_wrt_base base_loc tok], where [tok] represents a location
+  * relative to [base_loc], returns the same [tok] but with an absolute
+  * {! token_location}. This is useful for fixing parse info after
+  * {! Common2.with_tmp_file}. E.g. if [base_loc] points to line 3, and
+  * [tok] points to line 2 (interpreted line 2 starting in line 3), then
+  * the adjusted token will point to line 4. *)
+val adjust_info_wrt_base : token_location -> token_mutable -> token_mutable
 
 (*s: signature [[Parse_info.error_message]] *)
 val error_message : Common.filename -> (string * int) -> string

commit 282b9c4840166ea6d5c03d22bec23baf01ddf464

parse_info: Fix adjust_info_wrt_base
Fixes: 913836e07f5 ("parse_info: Add function to adjust token_mutable wrt a base location")

h_program-lang/Parse_info.ml
@@ -695,12 +695,13 @@ let mk_lexer_for_yacc toks is_comment =
   tr, lexer, lexbuf_fake
 
 let adjust_pinfo_wrt_base base_loc loc =
+  (* Note that charpos and columns are 0-based, whereas lines are 1-based. *)
   { loc with
-    charpos = base_loc.charpos + loc.charpos - 1;
+    charpos = base_loc.charpos + loc.charpos;
     line    = base_loc.line + loc.line - 1;
     column  =
       if loc.line = 1 then
-        base_loc.column + loc.column - 1
+        base_loc.column + loc.column
       else
         loc.column;
     file    = base_loc.file; }