a9eca4ad6246f84482912d529421915b0cdcd9c0
Martin Jambon <martin@mjambon.com>
2021-07-20 01:22:25 -0700

    Add missing functions to the Ls module

--- commons/Ls.ml
+++ commons/Ls.ml
@@ -6,6 +6,23 @@
 let map f l =
   List.rev_map f l |> List.rev
 
+(* List.rev_mapi isn't available *)
+let mapi f l =
+  let rec aux acc l =
+    match l with
+    | [] -> List.rev acc
+    | x :: l ->
+        let y = f x in
+        aux (y :: acc) l
+  in
+  aux [] l
+
+let map2 f a b =
+  List.rev_map2 f a b |> List.rev
+
+let append a b =
+  List.rev_append (List.rev a) b
+
 let flatten ll =
   match ll with
   | [] -> []
@@ -21,5 +38,60 @@ let fold_right f l acc =
   | l ->
       List.fold_left (fun acc l -> f l acc) acc (List.rev l)
 
-let append a b =
-  List.rev_append (List.rev a) b
+let fold_right2 f a b acc =
+  List.fold_left (fun acc a b -> f a b acc) acc (List.rev a) (List.rev b)
+
+let remove_assoc k l =
+  let rec aux acc = function
+    | [] -> List.rev acc
+    | ((k2, _) as kv) :: l ->
+        if k2 = k then
+          List.rev_append acc l
+        else
+          aux (kv :: acc) l
+  in
+  aux [] l
+
+(* Same as assoc but using '==' instead of '=' *)
+let remove_assoc k l =
+  let rec aux acc = function
+    | [] -> List.rev acc
+    | ((k2, _) as kv) :: l ->
+        if k2 == k then
+          List.rev_append acc l
+        else
+          aux (kv :: acc) l
+  in
+  aux [] l
+
+let rec rev_split al bl l =
+  match l with
+  | [] -> (al, bl)
+  | (a, b) :: l -> rev_split (a :: al) (b :: bl) l
+
+let split l =
+  List.rev l
+  |> rev_split []
+
+let rec rev_combine acc al bl =
+  match al, bl with
+  | [], [] -> acc
+  | a :: al, b :: bl -> rev_combine ((a, b) :: acc) al bl
+  | [], _ | _, [] -> invalid_arg "Ls.combine"
+
+let combine al bl =
+  rev_combine [] al bl
+  |> List.rev
+
+let merge cmp l1 l2 =
+  let rec aux acc l1 l2 =
+    match l1, l2 with
+    | [], l2 -> List.rev_append acc l2
+    | l1, [] -> List.rev_append acc l1
+    | h1 :: t1, h2 :: t2 ->
+        if cmp h1 h2 <= 0 then
+          aux (h1 :: acc) t1 l2
+        else
+          aux (h2 :: acc) l1 t2
+  in
+  aux [] l1 l2
--- commons/Ls.mli
+++ commons/Ls.mli
@@ -13,58 +13,68 @@
    At the time of writing, here's the popularity ranking of the List functions
    as used across pfff and semgrep-core:
 
-    875 List.map *
-    582 List.iter
-    408 List.rev
-    152 List.length
-    109 List.hd
-    108 List.fold_left
-    101 List.filter
-     79 List.flatten *
-     78 List.mem
-     62 List.assoc
-     47 List.tl
-     33 List.fold_right *
-     26 List.exists
-     24 List.for_all
-     20 List.partition
-     19 List.sort
-     18 List.rev_map
-     16 List.find
-     15 List.nth
-     12 List.concat * [alias for List.flatten]
-      8 List.rev_append
-      7 List.split
-      6 List.append * [(@) is usually used instead]
-      5 List.mem_assoc
-      2 List.assoc_opt
+   1800 List.map *
+    715 List.iter
+    471 List.rev
+    220 List.length
+    179 List.flatten *
+    157 List.fold_left
+    149 List.hd
+    127 List.filter
+     87 List.mem
+     65 List.assoc
+     49 List.tl
+     46 List.exists
+     35 List.fold_right *
+     33 List.for_all
+     22 List.sort
+     21 List.partition
+     21 List.concat * [alias for List.flatten]
+     20 List.rev_map
+     19 List.concat_map
+     17 List.find
+     16 List.nth
+     11 List.rev_append
+      9 List.filter_map
+      8 List.split *
+      8 List.assoc_opt
+      7 List.mapi *
+      7 List.append * [(@) is usually used instead]
+      6 List.mem_assoc
+      4 List.set
+      4 List.remove_assoc *
+      4 List.find_opt
+      2 List.map2 *
+      1 List.to_seq
+      1 List.sort_uniq
+      1 List.of_seq
+      1 List.nth_opt
+      1 List.init
+      1 List.for_all2
 
    * = unsafe
 
    Other unsafe functions:
-   - List.mapi
-   - List.map2
-   - List.fold_right2
-   - List.remove_assoc
    - List.remove_assq
-   - List.split
    - List.combine
    - List.merge
 *)
 
 val map : ('a -> 'b) -> 'a list -> 'b list
+val mapi : (int -> 'a -> 'b) -> 'a list -> 'b list
+val map2 : (int -> 'a -> 'b -> 'c) -> 'a list -> 'b list -> 'c list
+
+val append : 'a list -> 'a list -> 'a list
 val flatten : 'a list list -> 'a list
 val concat : 'a list list -> 'a list
-val fold_right : ('a -> 'b -> 'b) -> 'a list -> 'b -> 'b
-val append : 'a list -> 'a list -> 'a list
 
-(* TODO
-   val mapi
-   val map2
-   val fold_right2
-   val remove_assoc
-   val remove_assq
-   val split
-   val combine
-   val merge
-*)
+val fold_right : ('a -> 'b -> 'b) -> 'a list -> 'b -> 'b
+val fold_right2 : ('a -> 'b -> 'c -> 'c) -> 'a list -> 'b list -> 'c -> 'c
+
+val remove_assoc : 'a -> ('a * 'b) list -> ('a * 'b) list
+val remove_assoq : 'a -> ('a * 'b) list -> ('a * 'b) list
+
+val split : ('a * 'b) list -> 'a list * 'b list
+val combine : 'a list -> 'b list -> ('a * 'b) list
+
+val merge : ('a -> 'a -> int) -> 'a list -> 'a list -> 'a list
