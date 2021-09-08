a8860edb6b628876ab625a29bedc052e26a94ce3
Matthew McQuaid <matthew@returntocorp.com>
2021-09-08 09:35:48 -0700

    add TODO for label/goto in php_graph_code

--- lang_php/analyze/graph_code_php.ml
+++ lang_php/analyze/graph_code_php.ml
@@ -733,6 +733,7 @@ and stmt_bis env x =
       stmt env xs;
   | Return (_, eopt)  | Break (_, eopt) | Continue (_, eopt) ->
       Common2.opt (expr env) eopt
+  (* TODO: does anything special need to be done for Label and Goto? *)
   | Label (_,_,st) -> stmt env st
   | Goto _ -> ()
   | Throw (_, e) -> expr env e
