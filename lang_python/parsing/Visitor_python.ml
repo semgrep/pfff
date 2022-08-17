ce7f9117a06b4ddf30da07348618e6ca5d4a11b5
Nat Mote <nat@natmote.net>
2022-08-17 08:55:44 -0700

    Include start and end tokens of Python f strings
    Previously, these were discarded. Now they are included in the AST.
    
    Test plan: Incorporate these changes into Semgrep. Run existing
    automated tests. Manually verify that
    https://github.com/returntocorp/semgrep/issues/2995 is fixed.

--- lang_python/parsing/Visitor_python.ml
+++ lang_python/parsing/Visitor_python.ml
@@ -106,7 +106,11 @@ let (mk_visitor: visitor_in -> visitor_out) = fun vin ->
       | Num v1 -> let v1 = v_number v1 in ()
       | Str v1 -> let v1 = v_wrap v_string v1 in ()
       | EncodedStr (v1, v2) -> let v1 = v_wrap v_string v1 in let v2 = v_string v2 in ()
-      | InterpolatedString v1 -> let v1 = v_list v_expr v1 in ()
+      | InterpolatedString (v1, v2, v3) ->
+          let v1 = v_info v1 in
+          let v2 = v_list v_expr v2 in
+          let v3 = v_info v3 in
+          ()
       | ConcatenatedString v1 -> let v1 = v_list v_expr v1 in ()
       | Name (v1, v2, v3) ->
           let v1 = v_name v1
